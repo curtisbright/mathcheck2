cbde7095b18233cd564374aa9b10c7be878301f4
Curtis Bright <cbright@gmail.com>
2023-03-08 22:19:11 -0500

    Option to generate toplevel cubes

--- lams_problem/ovals/cube.sh
+++ lams_problem/ovals/cube.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-l=$1
+
+while getopts "t" opt
+do
+	case $opt in
+		t) l=$2; toplevel=" -l2 $((397-l)) -o2 cubes/b1-5.$l.simp.toplevel" ;;
+	esac
+done
+shift $((OPTIND-1))
 
 if [ -z $1 ]
 then
-	echo "Need case #"
+	echo "Need case # [run with -t as first argument to generate a collection of toplevel cubes]"
 	exit
 fi
+
+l=$1
 
 if [ ! -f cnf/b1-5.$l.simp.cnf ]
 then
@@ -13,7 +22,7 @@ then
 fi
 
 # Generate list of cubes for SAT instance
-command="./march_cu cnf/b1-5.$l.simp.cnf -o cubes/b1-5.$l.simp.cubes -n 684"
+command="./march_cu cnf/b1-5.$l.simp.cnf -o cubes/b1-5.$l.simp.cubes -n 684$toplevel"
 
 echo $command
 eval $command
